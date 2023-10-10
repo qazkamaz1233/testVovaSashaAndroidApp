a07fbab6a2c44e2f07f5e1d5a9653a7cfc6df161
Alexander <qazkamaz1@mail.ru>
2023-10-10 21:38:47 +0400

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -12,6 +12,8 @@ int main(int argc, char *argv[])
         &app, []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.load(url);
+
+    qDebug() << "ahhahaha";
 
     return app.exec();
 }
